587087169dbe0b1070542c8a38abde11010cd6a3
Manthan Dave <manthanhd@live.com>
2015-12-13 22:33:36 +0000

    Now fetching individual item when clicked live from api

--- www/js/app.js
+++ www/js/app.js
@@ -5,80 +5,85 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/')
-
-  $stateProvider
-  .state('home', {
-    url: '/',
-    controller: 'HomeController',
-    templateUrl: 'templates/home.html'
+        // Don't remove this line unless you know what you are doing. It stops the viewport
+        // from snapping when text inputs are focused. Ionic handles this internally for
+        // a much nicer keyboard experience.
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
   })
-  .state('county', {
-    url: '/county',
-    controller: 'CountyStationsController',
-    templateUrl: 'templates/county-stations.html'
+
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/')
+
+    $stateProvider
+      .state('home', {
+        url: '/',
+        controller: 'HomeController',
+        templateUrl: 'templates/home.html'
+      })
+      .state('county', {
+        url: '/county',
+        controller: 'CountyStationsController',
+        templateUrl: 'templates/county-stations.html'
+      })
+      .state('showItem', {
+        url: '/showItem',
+        controller: 'ItemDetailController',
+        templateUrl: 'templates/item-detail.html'
+      })
   })
-  .state('showItem', {
-    url: '/showItem',
-    controller: 'ItemDetailController',
-    templateUrl: 'templates/item-detail.html'
-  })
-})
 
-.filter('unique', function () {
+  .filter('unique', function () {
 
-  return function (items, filterOn) {
+    return function (items, filterOn) {
 
-    if (filterOn === false) {
-      return items;
-    }
+      if (filterOn === false) {
+        return items;
+      }
 
-    if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
-      var hashCheck = {}, newItems = [];
+      if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
+        var hashCheck = {}, newItems = [];
 
-      var extractValueToCompare = function (item) {
-        if (angular.isObject(item) && angular.isString(filterOn)) {
-          return item[filterOn];
-        } else {
-          return item;
-        }
-      };
+        var extractValueToCompare = function (item) {
+          if (angular.isObject(item) && angular.isString(filterOn)) {
+            if(filterOn.indexOf('.') !== -1) {
+              var res = filterOn.split(".");
+              return item[res[0]][res[1]];
+            }
 
-      angular.forEach(items, function (item) {
-        var valueToCheck, isDuplicate = false;
-
-        for (var i = 0; i < newItems.length; i++) {
-          if (angular.equals(extractValueToCompare(newItems[i]), extractValueToCompare(item))) {
-            isDuplicate = true;
-            break;
+            return item[filterOn];
+          } else {
+            return item;
           }
-        }
-        if (!isDuplicate) {
-          newItems.push(item);
-        }
+        };
 
-      });
-      items = newItems;
-    }
-    return items;
-  };
-});
+        angular.forEach(items, function (item) {
+          var valueToCheck, isDuplicate = false;
+
+          for (var i = 0; i < newItems.length; i++) {
+            if (angular.equals(extractValueToCompare(newItems[i]), extractValueToCompare(item))) {
+              isDuplicate = true;
+              break;
+            }
+          }
+          if (!isDuplicate) {
+            newItems.push(item);
+          }
+
+        });
+        items = newItems;
+      }
+      return items;
+    };
+  });
--- www/js/controller.js
+++ www/js/controller.js
@@ -1,20 +1,25 @@
 angular.module('starter')
-.controller('HomeController', function($scope, $http, $state) {
-	$http.get('http://environment.data.gov.uk/flood-monitoring/id/floodAreas?_limit=100').then(function(data, response) {
-		console.log(data);
-		$scope.$parent.items = data.data.items;
-	});
-	$scope.showItem = function(item) {
-		$scope.$parent.item = item;
-		$state.go("county");
-	};
-})
+  .controller('HomeController', function ($scope, $http, $state) {
+    $http.get('http://environment.data.gov.uk/flood-monitoring/id/floods').then(function (data, response) {
+      $scope.$parent.items = data.data.items;
+    });
+    $scope.showItem = function (item) {
+      $scope.$parent.item = item;
+      $state.go("county");
+    };
+  })
 
-.controller('CountyStationsController', function($scope, $http, $stateParams) {
-	var fwdCode = $stateParams.fwdCode;
-	$scope.item = $scope.$parent.item;
-	$scope.showItem = function(item) {
-		$scope.$parent.item = item;
-		$state.go("showItem");
-	}
-});
+  .controller('CountyStationsController', function ($scope, $http, $state) {
+    $scope.item = $scope.$parent.item;
+    $scope.showItem = function (item) {
+      $scope.$parent.item = item;
+      $state.go("showItem");
+    }
+  })
+
+  .controller('ItemDetailController', function ($scope, $http, $stateParams) {
+    var itemIdLink = $scope.$parent.item["@id"];
+    $http.get(itemIdLink).then(function(data) {
+      $scope.item = data.data.items;
+    });
+  });
