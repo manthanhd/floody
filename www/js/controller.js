354f75be7dbd004d365d02ff140234b731100824
Manthan Dave <manthanhd@live.com>
2016-01-12 06:39:48 +0000

    Added toggle functions to show/hide search + explainations for various flood severity levels.

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,5 +1,14 @@
 angular.module('starter')
   .controller('HomeController', function ($scope, $http, $state) {
+    $scope.showSearch = function() {
+      $scope.searchBar = true;
+    };
+
+    $scope.hideSearch = function() {
+      $scope.searchBar = false;
+      $scope.searchString = undefined;
+    };
+
     $http.get('http://environment.data.gov.uk/flood-monitoring/id/floods').then(function (data, response) {
       $scope.$parent.items = data.data.items;
     });
@@ -10,6 +19,15 @@ angular.module('starter')
   })
 
   .controller('CountyStationsController', function ($scope, $http, $state) {
+    $scope.showSearch = function() {
+      $scope.searchBar = true;
+    };
+
+    $scope.hideSearch = function() {
+      $scope.searchBar = false;
+      $scope.searchString = undefined;
+    };
+
     $scope.item = $scope.$parent.item;
     $scope.showItem = function (item) {
       $scope.$parent.item = item;
@@ -21,5 +39,16 @@ angular.module('starter')
     var itemIdLink = $scope.$parent.item["@id"];
     $http.get(itemIdLink).then(function(data) {
       $scope.item = data.data.items;
+
+      switch($scope.item.severityLevel) {
+        case 1:
+              $scope.explaination = "Severe flooding. Danger to life."; break;
+        case 2:
+              $scope.explaination = "Flooding is expected. Immediate action required"; break;
+        case 3:
+              $scope.explaination = "Flooding is possible. Be prepared"; break;
+        case 4:
+              $scope.explaination = "Flood warnings and flood alerts that have been removed in the last 24 hours."; break;
+      }
     });
   });
