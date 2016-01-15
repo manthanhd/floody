ffdb807f6ac6ced5a9af8d16151f1a1f82dd70b9
Manthan Dave <manthanhd@live.com>
2016-01-15 06:30:41 +0000

    Added local storage + ability to add/remove bookmarks for counties + toast functionality

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,5 +1,5 @@
 angular.module('starter')
-  .controller('HomeController', function ($scope, $http, $state, $ionicPopover) {
+  .controller('HomeController', function ($scope, $http, $state, $ionicPopover, $ionicListDelegate, ionicToast) {
     $scope.showSearch = function () {
       $scope.searchBar = true;
     };
@@ -12,9 +12,98 @@ angular.module('starter')
     $http.get('http://environment.data.gov.uk/flood-monitoring/id/floods').then(function (data, response) {
       $scope.$parent.items = data.data.items;
     });
+
     $scope.showItem = function (item) {
       $scope.$parent.item = item;
       $state.go("county");
+    };
+
+    $scope.isBookmarked = function(county) {
+      var bookmarks;
+      var storedBookmarks = window.localStorage["countyBookmarks"];
+      if(storedBookmarks) {
+        bookmarks = JSON.parse(storedBookmarks);
+      }
+
+      if(!bookmarks) {
+        bookmarks = [];
+        $scope.$parent.countyBookmarks = bookmarks;
+      }
+
+      for(var i = 0; i < bookmarks.length; i++) {
+        var bookmark = bookmarks[i];
+        if(bookmark === county) {
+          $ionicListDelegate.closeOptionButtons();
+          return i;
+        }
+      }
+
+      return undefined;
+    };
+
+    $scope.getBookmarks = function() {
+      var bookmarks;
+      var storedBookmarks = window.localStorage["countyBookmarks"];
+      if(storedBookmarks) {
+        bookmarks = JSON.parse(storedBookmarks);
+      }
+
+      if(!bookmarks) {
+        bookmarks = [];
+      }
+
+      return bookmarks;
+    };
+
+    $scope.persistBookmarks = function(bookmarks) {
+      $scope.$parent.countyBookmarks = bookmarks;
+
+      if(!bookmarks) {
+        window.localStorage["countyBookmarks"] = undefined;
+        return;
+      }
+
+      window.localStorage["countyBookmarks"] = JSON.stringify(bookmarks);
+    };
+
+    $scope.bookmarkCounty = function(county) {
+      var bookmarks;
+      var storedBookmarks = window.localStorage["countyBookmarks"];
+      if(storedBookmarks) {
+        bookmarks = JSON.parse(storedBookmarks);
+      }
+
+      if(!bookmarks) {
+        bookmarks = [];
+        $scope.$parent.countyBookmarks = bookmarks;
+      }
+
+      for(var i = 0; i < bookmarks.length; i++) {
+        var bookmark = bookmarks[i];
+        if(bookmark === county) {
+          $ionicListDelegate.closeOptionButtons();
+          return;
+        }
+      }
+
+      bookmarks.push(county);
+      $scope.$parent.countyBookmarks = bookmarks;
+      window.localStorage["countyBookmarks"] = JSON.stringify(bookmarks);
+      $ionicListDelegate.closeOptionButtons();
+      ionicToast.show(county + " added to bookmarks.", "bottom", false, 2500);
+    };
+
+    $scope.removeBookmark = function(county) {
+      var location = $scope.isBookmarked(county);
+      if(!location) {
+        return;
+      }
+
+      var bookmarks = $scope.getBookmarks();
+      bookmarks.splice(location, 1);
+      $scope.persistBookmarks(bookmarks);
+      console.log(bookmarks);
+      ionicToast.show(county + " removed from bookmarks.", "bottom", false, 2500);
     };
 
     $ionicPopover.fromTemplateUrl('templates/popover-menu.html', {
